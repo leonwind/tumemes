60f4236a49f1923cb56727885b09a48a64652c77
Leon Windheuser <leon.windheuser@gmail.com>
2020-06-19 13:14:33 +0200

    Refactor authentication

---
+++ service/src/main/java/auth/Hashing.java
@@ -0,0 +1,36 @@
+package auth;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Random;
+
+public class Hashing {
+  private static final int NUM_ITERATIONS = 65536; // 63336 = 2^16
+  private static final int SALT_LENGTH = 16;
+  private static final int KEY_LENGTH = 128;
+
+  private static byte[] convertStringToByteArray(String s) {
+    return s.getBytes();
+  }
+
+  public static byte[] generateSalt() {
+    byte[] salt = new byte[SALT_LENGTH];
+    new Random().nextBytes(salt);
+    return salt;
+  }
+
+  public static byte[] generateHash(String password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt,
+        NUM_ITERATIONS, KEY_LENGTH);
+
+    SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+    return f.generateSecret(spec).getEncoded();
+  }
+
+  public static byte[] generateHash(String password, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException{
+    return generateHash(password, convertStringToByteArray(salt));
+  }
+}
--- service/src/main/java/resources/RegisterResource.java
+++ service/src/main/java/resources/RegisterResource.java
@@ -2,19 +2,16 @@ package resources;
 
 import accessors.UserDAO;
 import api.RegisterService;
+import auth.Hashing;
 import core.NewUser;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
 import java.util.Base64;
-import java.util.Random;
 
 public class RegisterResource implements RegisterService {
 
@@ -34,33 +31,20 @@ public class RegisterResource implements RegisterService {
       return Response.status(400).entity("Email does already exists").build();
     }
 
-    final int NUM_ITERATIONS = 65536; // 65536 = 2^16
-    final int SALT_LENGTH = 16;
-    final int KEY_LENGTH = 128;
-
-    // generate salt
-    byte[] salt = new byte[SALT_LENGTH];
-    new Random().nextBytes(salt);
-
-    KeySpec spec =
-        new PBEKeySpec(newUser.getPassword().toCharArray(), salt, NUM_ITERATIONS, KEY_LENGTH);
+    byte[] salt = Hashing.generateSalt();
 
     try {
-      SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-      byte[] hash = f.generateSecret(spec).getEncoded();
+      byte[] hash = Hashing.generateHash(newUser.getPassword(), salt);
 
       Base64.Encoder enc = Base64.getEncoder();
 
-      userDAO.addNewUser(
-          newUser.getName(),
-          newUser.getEmail(),
-          enc.encodeToString(hash),
-          enc.encodeToString(salt));
+      userDAO.addNewUser(newUser.getName(), newUser.getEmail(),
+          enc.encodeToString(hash), enc.encodeToString(salt));
 
-      return Response.ok().build();
     } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
       e.printStackTrace();
       return Response.status(400).build();
     }
+    return Response.ok("Added new user").build();
   }
 }
